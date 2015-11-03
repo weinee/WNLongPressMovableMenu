604acb7b50ac236911c7a3b2a94950c3eac809d6
cpr <cpr@zhangwendeMac-mini.local>
2015-11-03 09:33:42 +0800

    Initial Commit

---
+++ 自定义导航菜单/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义导航菜单
+//
+//  Created by cpr on 15/11/3.
+//  Copyright (c) 2015年 scsys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 自定义导航菜单/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义导航菜单
+//
+//  Created by cpr on 15/11/3.
+//  Copyright (c) 2015年 scsys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
